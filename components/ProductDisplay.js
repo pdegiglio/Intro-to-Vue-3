931371d3653c714413f0512e5209b694de473f04
pdg784 <pdg784@gmail.com>
2021-12-19 23:51:10 +0100

    finished L10

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -35,9 +35,16 @@ app.component('product-display', {
         <button 
           class="button" 
           :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
+          
           v-on:click="addToCart">
           Add to Cart
+        </button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          
+          v-on:click="removeFromCart">
+          Remove From Cart
         </button>
       </div>
     </div>
@@ -56,7 +63,10 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      removeFromCart() {
+          this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
